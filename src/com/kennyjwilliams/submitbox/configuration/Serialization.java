65307193229b1437fe3b1ef4539417984f3d9cf4
Kenny Williams <kenny@kennyjwilliams.com>
2014-03-30 13:34:03 -0700

    Setups up the FTP using serialization

--- src/com/kennyjwilliams/submitbox/configuration/Serialization.java
+++ src/com/kennyjwilliams/submitbox/configuration/Serialization.java
@@ -1,6 +1,8 @@
 package com.kennyjwilliams.submitbox.configuration;
 
 import com.kennyjwilliams.submitbox.framework.Course;
+import com.kennyjwilliams.submitbox.framework.ftp.Downloader;
+import com.kennyjwilliams.submitbox.framework.ftp.FTP;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -26,7 +28,7 @@ public class Serialization
     {
         try
         {
-            File f = new File("courses"+File.separator+c.getName()+".course");
+            File f = new File("courses"+File.separator+c.getName()+".sbt");
             if(!f.exists())
             {
                 f.getParentFile().mkdirs();
@@ -48,7 +50,7 @@ public class Serialization
      * @param path
      * @return
      */
-    public static Course deserialize(String path)
+    public static Course deserializeCourse(String path)
     {
         Course c = null;
         try
@@ -66,5 +68,55 @@ public class Serialization
             Logger.getLogger(Serialization.class.getName()).log(Level.SEVERE, null, ex);
         }
         return c;
+    }
+    
+    /**
+     * Serializes a course to the default course save directory
+     * @param d The course to be serialized 
+     */
+    public static void serialize(FTP d)
+    {
+        try
+        {
+            File f = new File("settings"+File.separator+"settings.sbt");
+            if(!f.exists())
+            {
+                f.getParentFile().mkdirs();
+                f.createNewFile();
+            }
+            FileOutputStream fos = new FileOutputStream(f);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(d);
+            oos.close();
+            fos.close();
+        }catch(IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+    
+    /**
+     * 
+     * @param path
+     * @return
+     */
+    public static FTP deserializeFTP(String path)
+    {
+        FTP d = null;
+        try
+        {
+            FileInputStream fis = new FileInputStream(path);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            d = (FTP) ois.readObject();
+            ois.close();
+            fis.close();
+        } catch (FileNotFoundException ex)
+        {
+            Logger.getLogger(Serialization.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException | ClassNotFoundException ex)
+        {
+            Logger.getLogger(Serialization.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return d;
     }
 }
