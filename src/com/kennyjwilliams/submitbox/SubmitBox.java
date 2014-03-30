957013a77b40061ae29e4741e71b3a5215e2d61d
Kenny Williams <kenny@vectorgaming.net>
2014-03-30 16:45:22 -0700

    Removed TestFTP. Added SubmitBox class

---
+++ src/com/kennyjwilliams/submitbox/SubmitBox.java
@@ -0,0 +1,15 @@
+package com.kennyjwilliams.submitbox;
+
+import com.kennyjwilliams.submitbox.admin.AdminPanel;
+
+/**
+ *
+ * @author Kenny
+ */
+public class SubmitBox 
+{
+    public static void main(String[] args)
+    {
+        new AdminPanel();
+    }
+}
